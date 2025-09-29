b4856b0bb8ff09981ba4a6f7ab54d7a1099aa425
Sven65 <Sven65@users.noreply.github.com>
2025-09-29 22:29:45 +0200

    what

--- src/main/java/xyz/mackan/crystallurgy/blocks/EnergySyncableBlockEntity.java
+++
@@ -1,9 +0,0 @@
-package xyz.mackan.crystallurgy.blocks;
-
-import net.minecraft.block.entity.BlockEntity;
-import team.reborn.energy.api.base.SimpleEnergyStorage;
-
-public interface EnergySyncableBlockEntity {
-    public final SimpleEnergyStorage energyStorage = null;
-    public void setEnergyLevel(long energyLevel);
-}
